39176d38b9176eb942b8cb819bb6c7baaf8b2479
Julien M <jmarchand@leadformance.com>
2012-12-27 23:21:16 +0100

    Added tray icon, hid the main window and show it through tray menu

--- source/server/index.js
+++ source/server/index.js
@@ -10,3 +10,56 @@ var settings = require('./server/settings');
 var settingsPath = '/Users/julien/Dropbox/dev/NodeJS/Bridget';
 settings.file(settingsPath + '/_bridget_settings.json');
 
+
+/*
+NATIVE UI - TRAY, MENU
+*/
+
+  // Load native UI library
+  var gui = require('nw.gui');
+
+  // Get the current window
+  var win = gui.Window.get();
+
+  win.on('close', function() {
+    win.hide(); // Pretend to be closed already
+    console.log("We're closing...");
+    //this.close(true);
+  });
+
+  // Create a tray icon
+  var tray = new gui.Tray({
+    //title: 'Bridget',
+    icon: 'source/img/tray-icon.png'
+  });
+
+  // Give it a menu
+  var menu = new gui.Menu();
+  menu.append(new gui.MenuItem({
+    label: 'Watch a folder...',
+    click: function(){
+      console.log("I'm clicked");
+      //win.open('source/list.html');
+      win.show();
+      win.focus();
+    }
+  }));
+  menu.append(new gui.MenuItem({type:"separator"}));
+  menu.append(new gui.MenuItem({
+    label: 'Quit',
+    click: function(){
+      console.log("Bye!");
+      win.hide();
+      // here, code to unwatch folders
+      // TODO
+
+      // Quit current app
+      gui.App.quit();
+    }
+  }));
+
+  tray.menu = menu;
+
+  // Remove the tray
+  // tray.remove();
+  // tray = null;
