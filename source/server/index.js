3154c804f43120836215e0390ac82afb4f2a9e2f
Julien M <jmarchand@leadformance.com>
2012-12-27 02:32:16 +0100

    Externalized server-side JS

---
+++ source/server/index.js
@@ -0,0 +1,13 @@
+/*
+NCONF - SAVE CONF
+*/
+
+// require local Settings package
+var settings = require('./server/settings');
+
+// set the settings file to use
+// var settingsPath = process.env['HOME'];
+var settingsPath = '/Users/julien/Dropbox/dev/NodeJS/Bridget';
+settings.file(settingsPath + '/_bridget_settings.json');
+
+console.log("index.js loaded");
