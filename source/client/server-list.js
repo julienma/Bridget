c43b2a24fb3b2787af750ad675043eb63245fb53
Julien M <jmarchand@leadformance.com>
2013-01-08 00:59:12 +0100

    Added auth (login:pwd) in server URLs. Blank for now.

--- source/client/server-list.js
+++ source/client/server-list.js
@@ -1,23 +1,27 @@
+// TODO: see if there's any need to auth. If so, make sure to externalize this in a json (loaded through nconf), and .gitignore it
+var authLogin = '';
+var authPwd = '';
+
 // Define URLs for known API servers
 var apiServer = [
   {
     id:'q',
-    url:'https://api-q.leadformance.com',
+    url:'https://' + authLogin + ':' + authPwd + '@api-q.leadformance.com',
     name:'Internal QA (.q)'
   },
   {
     id:'c',
-    url:'https://api-c.leadformance.com',
+    url:'https://' + authLogin + ':' + authPwd + '@api-c.leadformance.com',
     name:'Client QA (.c)'
   },
   {
     id:'i',
-    url:'https://api-i.leadformance.com',
+    url:'https://' + authLogin + ':' + authPwd + '@api-i.leadformance.com',
     name:'Integrator (.i)'
   },
   {
     id:'s',
-    url:'https://api-s.leadformance.com',
+    url:'https://' + authLogin + ':' + authPwd + '@api-s.leadformance.com',
     name:'Staging (.s)'
   }
 ];
@@ -25,6 +29,7 @@ var apiServer = [
 /*
 Find details (url, name) about a server, given its id ('c', 'q', etc.)
 Usage: console.log(getServerDetails('c')[0].url);
+SIMILAR to NJSgetServerDetails() in server/template.js, but needs jQuery (and wouldn't work as is within NodeJS)
 */
 function getServerDetails(id){
   return $.grep(apiServer, function(item){
