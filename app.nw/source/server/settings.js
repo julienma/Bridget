julienma/Bridget

commit 137a6d51f44790aa2757f8fef84e7daa3dd43ff7

Export loadedSettings so it is public to other modules
And clean the useless parameters forwarded from functions to functions.

app.nw/source/server/settings.js
@@ -4,7 +4,10 @@ var watch = require('./watch.js');
 // require local Tray package (node-webkit's tray)
 var trayMenu = require('./traymenu.js');
 
+// hold current settings.json file name
 var currentSettingsFile;
+// public array of settings, loaded from json
+var loadedSettings = [{}];
 
 function file (f){
   nconf.file(f);
@@ -13,7 +16,6 @@ function file (f){
 
 function read (callback){
   nconf.load(function () {
-    var loadedSettings = [{}];
     var i = -1;
 
     var conf = nconf.get();
@@ -123,7 +125,7 @@ function loadAndWatchFolders() {
       // for each watched folder
       for (var i=0; i<loadedSettings.length-1; i++) {
         // add one tray menu entry
-        trayMenu.addWatchedFolder(loadedSettings, i);
+        trayMenu.addWatchedFolder(i);
         // get every paths loaded
         pathsToWatch.push(loadedSettings[i]['path']);
       }
@@ -131,7 +133,7 @@ function loadAndWatchFolders() {
     // make sure we reset watched folders (if any)
     watch.stop();
     // and watch paths
-    watch.start(pathsToWatch, loadedSettings);
+    watch.start(pathsToWatch);
   });
 }
 
@@ -140,3 +142,4 @@ exports.read = read;
 exports.save = save;
 exports.clear = clear;
 exports.loadAndWatchFolders = loadAndWatchFolders;
+exports.loadedSettings = loadedSettings;

app.nw/source/server/template.js
@@ -11,6 +11,8 @@ var serverlist = require('../client/serverlist.js');
 var trayMenu = require('./traymenu.js');
 // require local Notification package
 var notification = require('./notification.js');
+// require local Settings package (nconf)
+var settings = require('./settings.js');
 
 // a lock is used to create an interval between multiple file changes / uploads
 var locked = 0;
@@ -34,20 +36,20 @@ function isLocked () {
   return locked;
 }
 
-function upload (filePath, loadedSettings) {
+function upload (filePath) {
   // set a lock so we avoid uploading more than once
   lock();
 
   console.log("TEMPLATE: " + filePath);
 
   // try to find the template directory within the filePath of changed file
-  for (var i=0; i<loadedSettings.length-1; i++) {
-    if (filePath.indexOf(loadedSettings[i]['path']) !=-1) {
-      var templateDir = loadedSettings[i]['path'];
-      var templateName = loadedSettings[i]['templateName'];
+  for (var i=0; i<settings.loadedSettings.length-1; i++) {
+    if (filePath.indexOf(settings.loadedSettings[i]['path']) !=-1) {
+      var templateDir = settings.loadedSettings[i]['path'];
+      var templateName = settings.loadedSettings[i]['templateName'];
       // construct the "template upload" API Url
-      var uploadUrl = serverlist.getServerDetails(loadedSettings[i]['serverId'], serverlist.apiServer).url + '/templates/' + loadedSettings[i]['templateId'] + '.json?oauth_token=' + loadedSettings[i]['apiKey'];
-      console.log('FOUND path for template ' + loadedSettings[i]['templateName'] + ': ' + uploadUrl);
+      var uploadUrl = serverlist.getServerDetails(settings.loadedSettings[i].serverId, serverlist.apiServer).url + '/templates/' + settings.loadedSettings[i].templateId + '.json?oauth_token=' + settings.loadedSettings[i].apiKey;
+      console.log('FOUND path for template ' + settings.loadedSettings[i].templateName + ': ' + uploadUrl);
       // wait a bit that user's done all the filesystem operations before continuing
       setTimeout(function() {
         zipAndUpload(templateDir, uploadUrl, templateName);

app.nw/source/server/traymenu.js
@@ -42,7 +42,7 @@ function removeWatchedFolders() {
   }
 }
 
-function addWatchedFolder(loadedSettings, position){
+function addWatchedFolder(position){
   // add a separator as first item if this is the 1st watched folder
   if (position === 0) {
     menu.insert(new gui.MenuItem({type:"separator"}), trayPositionForWatchedFolders);
@@ -54,7 +54,7 @@ function addWatchedFolder(loadedSettings, position){
   submenu.append(new gui.MenuItem({
     label: 'Open folder...',
     click: function() {
-      gui.Shell.showItemInFolder(loadedSettings[position].path);
+      gui.Shell.showItemInFolder(settings.loadedSettings[position].path);
     }
   }));
 
@@ -62,7 +62,7 @@ function addWatchedFolder(loadedSettings, position){
   submenu.append(new gui.MenuItem({
     label: 'Zip & Upload template',
     click: function() {
-      require('./template.js').upload(loadedSettings[position].path, loadedSettings);
+      require('./template.js').upload(settings.loadedSettings[position].path, settings.loadedSettings);
     }
   }));
 
@@ -72,10 +72,10 @@ function addWatchedFolder(loadedSettings, position){
     label: 'Unwatch',
     click: function() {
       // confirm?
-      var doRemove = window.confirm('Are your sure you want to remove this watched folder?\n' + loadedSettings[position].path);
+      var doRemove = window.confirm('Are your sure you want to remove this watched folder?\n' + settings.loadedSettings[position].path);
       // if confirmed, clear 'path' key from settings, and reload everything
       if (doRemove){
-        settings.clear(loadedSettings[position].path, function (err){
+        settings.clear(settings.loadedSettings[position].path, function (err){
           console.log('UNWATCH OK');
           // reset the watched folders from tray menu, so we can reload them again
           removeWatchedFolders();
@@ -88,18 +88,18 @@ function addWatchedFolder(loadedSettings, position){
 
   // get the last folder from the path.
   // require('path').sep is platform-specific file separator. '\\' or '/'.
-  var folder = '/' + loadedSettings[position].path.split(require('path').sep).splice(-1,1);
+  var folder = '/' + settings.loadedSettings[position].path.split(require('path').sep).splice(-1,1);
 
   var item = new gui.MenuItem({
-    label: folder + '  >  ' + loadedSettings[position].templateName,
-    tooltip: loadedSettings[position].path,
-    icon: 'source/img/server-icon-' + loadedSettings[position].serverId + '.png',
+    label: folder + '  >  ' + settings.loadedSettings[position].templateName,
+    tooltip: settings.loadedSettings[position].path,
+    icon: 'source/img/server-icon-' + settings.loadedSettings[position].serverId + '.png',
     submenu: submenu
   });
   // add the new tray menu item, after separator (+1)
   menu.insert(item, trayPositionForWatchedFolders + position + 1);
 
-  // console.log("Menu - " + 'path: ' + loadedSettings[position]['path'] + ' - serverId: ' + loadedSettings[position]['serverId'] + ' - apiKey: ' + loadedSettings[position]['apiKey'] + ' - templateId: ' + loadedSettings[position]['templateId'] + ' - templateName: ' + loadedSettings[position]['templateName']);
+  // console.log("Menu - " + 'path: ' + settings.loadedSettings[position]['path'] + ' - serverId: ' + settings.loadedSettings[position]['serverId'] + ' - apiKey: ' + settings.loadedSettings[position]['apiKey'] + ' - templateId: ' + settings.loadedSettings[position]['templateId'] + ' - templateName: ' + settings.loadedSettings[position]['templateName']);
 }
 
 function create() {

app.nw/source/server/watch.js
@@ -8,7 +8,7 @@ var notification = require('./notification.js');
 // make watcher globally available (dirty trick)
 var watchJob = {};
 
-function start (pathsToWatch, loadedSettings) {
+function start (pathsToWatch) {
     // Watch a directory or file
     console.log("Start watching for paths: " + pathsToWatch);
 
@@ -32,7 +32,7 @@ function start (pathsToWatch, loadedSettings) {
                 if ((filePath.indexOf(global.zipfile) !=-1) || template.isLocked()) {
                     console.log('WATCHR: Locked / Ignored file');
                 } else {
-                    template.upload(filePath, loadedSettings);
+                    template.upload(filePath);
                     console.log('WATCHR: Upload template!');
                 }
             }

commit 529dab329e4a0d4733a55bb93eb1d3858440802b

Clear loadedSettings before reloading it
Fix an issue introduced in 137a6d51f44790aa2757f8fef84e7daa3dd43ff7
(each time we would reload the settings, it would be appended instead
or replacing loadedSettings)

app.nw/source/server/settings.js
@@ -16,6 +16,8 @@ function file (f){
 
 function read (callback){
   nconf.load(function () {
+    // clear loadedSettings before reloading it
+    loadedSettings.length = 1;
     var i = -1;
 
     var conf = nconf.get();